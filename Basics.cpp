25eec958e4522bad4588b0f247f7ff91d3fd6b5e
dikshabhor791 <dikshabhor79@gmail.com>
2025-09-14 10:58:38 +0530

    Add Basics.cpp File

---
+++ Basics.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+using namespace std;
+int main(){
+//  Implicit Conversion
+// char grade = 'A';
+// double no = grade;
+// cout << no;
+
+// Explicit conversion
+// double no = 39.89;
+// int no1 = int(no);
+// cout<<no1;
+
+// operators
+
+// Arithmatics
+// int a = 5 ,b = 4;
+// cout << "Addition : " << (a+b) << endl; 
+// cout << "Substraction : " << (a-b) << endl; 
+// cout << "Multification : " << (a*b) << endl; 
+// cout << "Division : " << (a/b) << endl;
+// cout << "Division : " << (a/double(b)) << endl; 
+// cout << "Modulo : " << (a%b) << endl; 
+
+// Relation
+// int a = 5 , b=6;
+// cout << (a<b) << endl; // true
+// cout << (a>b) << endl; //false
+// cout << (a<=b) << endl; //true
+// cout << (a>=b) << endl; //false
+// cout << (a==b) << endl; //false
+// cout << (a!=b) << endl; //false
+
+// logical
+
+// int a = 5 , b=6;
+//  cout << ! (a<b) << endl; 
+//  cout <<((a<b) && (a>b)) << endl; //false
+//  cout <<((a<b) || (a>b)) << endl; //true
+
+// conditional statements
+// int n = 3;
+//  if(n >= 0){
+//   cout << " n is positive";
+//  }else{
+//     cout << "n is negative";
+//  }
+
+//    int marks = 75;
+//    if((marks >= 80) && (marks <= 90)){
+//     cout << "A";
+//    }else if ((marks >=70 ) && (marks <= 80)){
+//     cout << "B";
+//    } else {
+//     cout<<"C";
+//    }
+
+    //   char ch = 'A';
+    //   if((ch >= 'A') && (ch<= 'Z'))
+    //   {
+    //     cout<< "Uppercase";
+    //   }else {
+    //     cout << "Lowercase";
+    //   }
+
+
+    // int n = 1;
+    // while(n <= 10){
+    //     cout << n << endl;
+    //     // n++;
+    // }
+
+    // int n =10 ;
+    // for(int i=1 ; i<=10 ;i++){
+    //     cout<<i;
+    // }
+
+    // sum of numbers
+    // int n =5;
+    // int sum = 0;
+    // for(int i=0 ; i<= n ; i++){
+    //     sum = sum + i;
+    // }
+    // cout<<sum;
+
+    //  int n =5;
+    // int sum = 0;
+    // for(int i=0 ; i<= n ; i++){
+    //     if( i%2 != 0){
+    //     sum = sum + i;
+    //     }
+    // }
+    // cout<<sum;
+
+    // int n = 1;
+    // do{
+    //     cout << n<<" ";
+    //     n++;
+    // }while(n <= 10);
+
+    int n ;
+    int isboolean = true;
+    int fact =0;
+    for(int i=0 ; i<=10 ; i++){
+        if(i% n == 0){
+        isboolean=true;  
+        break;
+        }else if
+    }
+    return 0;
+}
