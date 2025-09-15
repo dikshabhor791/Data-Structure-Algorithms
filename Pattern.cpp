ac8adc99dc56ccda2374f784b62f7f589a48fa38
dikshabhor791 <dikshabhor79@gmail.com>
2025-09-15 22:30:02 +0530

    Add reverse triangle pattern &floyds triangle pattern & Pyramid pattern

--- Pattern.cpp
+++ Pattern.cpp
@@ -65,17 +65,207 @@ int main(){
 //     cout <<endl;
 // }
 
-int ch='A';
-int n=3;
-for(int i = 0; i<n; i++){
-    for(int j=1 ; j<=n; j++){
-        cout << ch;
-        ch++;
-        
-    }
-    cout <<endl;
-}
 
+// ABC
+// DEF
+// GHI
+// char ch='A';
+// int n=3;
+// for(int i = 0; i<n; i++){
+//     for(int j=1 ; j<=n; j++){
+//         cout << ch;
+//         ch++;
+        
+//     }
+//     cout <<endl;
+// }
+
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// int n = 6;
+// for(int i=0; i<=n; i++){
+//     for(int j=0; j<=i; j++){
+//        cout << "* ";
+//     }
+//     cout<<endl;
+// }
+
+// 1 
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// int n ;
+// cout<<"Enter n:";
+// cin>>n;
+// int num =1;
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<=i; j++){
+//         cout<<num << " ";
+//     }
+//     num++;
+//     cout<<endl;
+// }
+
+
+// A 
+// B B
+// C C C
+// D D D D
+// char ch= 'A';
+// int n=4;
+// for(int i=0; i<n; i++){
+   
+//     for(int j=0; j<=i; j++){
+//         cout<<ch<<" ";
+//     }
+//     ch++;
+//     cout<<endl;
+// }
+
+// 1 
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// int n = 4;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         cout<<j<<" ";
+//     }
+//     cout<< endl;
+// }
+
+// A 
+// A B
+// A B C
+// A B C D
+
+// int n = 4;
+// for(int i=1;i<=n;i++){
+//     char ch='A';
+//     for(int j=1;j<=i;j++){
+//         cout<<ch<<" ";
+//         ch++;
+//     }
+//     cout<< endl;
+// }
+
+// 1 
+// 2 1
+// 3 2 1
+// 4 3 2 1
+// int n=4;
+// for(int i=0; i<=n; i++){
+//     for(int j=i; j>0; j--){
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// 1 
+// 2 3
+// 4 5 6
+// 7 8 9 10 
+// int n=4;
+// int num=1;
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<i+1; j++){
+//         cout<<num<<" ";
+//         num++;
+//     }
+//     cout<<endl;
+// }
+
+
+// A 
+// B C
+// D E F
+// G H I J
+// char ch='A';
+// int n=4;
+// for(int i=0;i<n;i++){
+//     for(int j=0; j<i+1; j++){
+//         cout<<ch<<" ";
+//         ch++;
+//     }
+//     cout<<endl;
+// }
+
+//    A 
+//    B A 
+//    C B A 
+//    D C B A
  
+// int n=4;
+// for(int i=0; i<n; i++){
+//     for(char ch='A'+i; ch>='A'; ch--){
+//         cout<<ch << " ";  
+//     }
+   
+//     cout<<endl;
+// }
+
+// 1111
+//  222
+//   33
+//    4
+// int n=4;
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<i; j++){
+//         cout<<" ";
+//     }
+//     for(int j=0; j<n-i; j++){
+//         cout<<i+1 ;
+//     }
+//     cout<<endl;
+// }
+
+
+
+// AAAA
+//  BBB
+//   CC
+//    D
+// char ch='A';
+// int n=4;
+// for(int i=0; i<n; i++){
+//     for(int j=0; j<i; j++){
+//         cout<<" ";
+//     }
+//     for(int j=0; j<n-i; j++){
+//         cout<<ch ;
+//     }
+//     ch++;
+//     cout<<endl;
+// }
+
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+// int n = 5;
+// for(int i=0; i<n; i++){
+//    for(int j=0; j<n-i-1; j++){
+//      cout<<" ";
+//    }
+//    for(int j=1;j<=i+1; j++){
+//     cout<<j;
+//    }
+   
+//    for(int j=i; j>=1; j--){
+//     cout<<j ;
+//    }
+//    cout<<endl;
+// }
+
+
+
     return 0;
 }
